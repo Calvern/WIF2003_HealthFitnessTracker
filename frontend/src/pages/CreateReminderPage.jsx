7b749bb4f61b593942d673b6385bd51e184d9d83
Vambot07 <salihinazizizol@gmail.com>
2025-05-06 15:03:07 +0800

    Add Notifications module

--- frontend/src/pages/CreateReminderPage.jsx
+++ frontend/src/pages/CreateReminderPage.jsx
@@ -1,8 +1,17 @@
-
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { ArrowLeft } from "react-bootstrap-icons";
 
 const CreateReminderPage = () => {
+  const [validated, setValidated] = useState(false);
+
   return (
-    <h1>CreateReminderPage</h1>
+    <>
+      <div className="d-flex align-items-center ml-80 mt-5" style={{ fontWeight: "bold", fontSize: "30px" }}>
+        <ArrowLeft style={{marginLeft: '40px', marginRight:"40px"}}/>
+        Create New Reminder
+      </div>
+    </>
   )
 }
 
