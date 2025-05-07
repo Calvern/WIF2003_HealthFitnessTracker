0abd0e19c3382d54289bff0e336184b2cf6e5252
Calvern <user@Calvern-MacBookAir.local>
2025-05-07 17:46:52 +0800

    Completed add to diary and added toast

--- frontend/src/pages/FavouriteMealPage.jsx
+++ frontend/src/pages/FavouriteMealPage.jsx
@@ -1,6 +1,7 @@
 import { Container, Pagination } from "react-bootstrap";
 import MealCard from "../components/Nutrition/MealCard";
 import { useState } from "react";
+import AddMealModal from "../components/Nutrition/AddMealModal";
 
 const FavouriteMealPage = () => {
   const [activePage, setActivePage] = useState(1);
@@ -8,12 +9,19 @@ const FavouriteMealPage = () => {
   for (let number = 1; number <= 5; number++) {
     items.push(number);
   }
+
+  const [showAddDiary, setShowDiary] = useState(false);
+  const handleCloseAddDiary = () => setShowDiary(false);
+  const handleShowAddDiary = () => setShowDiary(true);
+
   return (
     <Container className="mt-5">
+      <AddMealModal show={showAddDiary} handleClose={handleCloseAddDiary} />
+
       <h2 className="fw-bold ">Your favourites meals</h2>
-      <MealCard />
-      <MealCard />
-      <MealCard />
+      <MealCard handleShowAddDiary={handleShowAddDiary} />
+      <MealCard handleShowAddDiary={handleShowAddDiary} />
+      <MealCard handleShowAddDiary={handleShowAddDiary} />
       <div className="w-100 d-flex justify-content-center">
         <Pagination className="mt-3">
           {items.map((pageNumber) => (
