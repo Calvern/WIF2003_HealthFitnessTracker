2041c55310f068ec747d1fa4d0de3eb2b77ca6f8
pohsharon <pohsharon8855@gmail.com>
2025-05-08 14:21:03 +0800

    completed modal

--- frontend/src/pages/LogCardioPage.jsx
+++ frontend/src/pages/LogCardioPage.jsx
@@ -2,14 +2,35 @@ import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 import SearchBar from "../components/Nutrition/SearchBar";
 import CardioList from "../components/Fitness/CardioList";
 import { Plus } from "react-bootstrap-icons";
-
+import { useState } from "react";
+import LogCardioModal from "../components/Fitness/LogCardioModal";
 
 const LogCardioPage = () => {
+  const [show, setShow] = useState(false);
+  const [selectedCardio, setSelectedCardio] = useState("");
+  const [log, setLog] = useState({
+    date: "",
+    time: "",
+    duration: "",
+  });
+
+  const handleItemClick = (cardio) => {
+    setSelectedCardio(cardio);
+    setShow(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Cardio:", selectedCardio, log);
+    setShow(false);
+    setLog({ date: "", time: "", duration: "" });
+  };
+
   return (
     <Container className="py-5">
       <div className="d-flex align-items-center gap-3 mb-1">
         <h2 className="fw-bold mb-0">Cardio Categories</h2>
-        <Button
+        {/* <Button
           style={{
             backgroundColor: "#507DBC", // Blue color
             border: "none",
@@ -23,10 +44,18 @@ const LogCardioPage = () => {
           }}
         >
           <Plus size={18} color="white" />
-        </Button>
+        </Button> */}
       </div>
       <SearchBar width="100%" />
-      <CardioList />
+      <CardioList onItemClick={handleItemClick} />
+      <LogCardioModal
+        show={show}
+        onClose={() => setShow(false)}
+        cardio={selectedCardio}
+        log={log}
+        setLog={setLog}
+        onSubmit={handleSubmit}
+      />
     </Container>
   );
 };
